f26f2ed7bdaf07b16f937629ea6c865a75837aa4
Chris Kentfield <chris@unstuckwebdev.com>
2022-08-13 12:37:10 -0400

    initial import

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+extern crate web_sys;
+
+mod utils;
+
+use wasm_bindgen::prelude::*;
+
+// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// allocator.
+#[cfg(feature = "wee_alloc")]
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, tree-sitter-exploration!");
+}
+// A macro to provide `println!(..)`-style syntax for `console.log` logging.
+macro_rules! log {
+    ( $( $t:tt )* ) => {
+        web_sys::console::log_1(&format!( $( $t )* ).into());
+    }
+}
